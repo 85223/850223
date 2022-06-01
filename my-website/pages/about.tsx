c8ad9e24fe1cc7b31d6ee8101da1a650cac43566
yuhsiang <H24364032@gmail.com>
2022-06-01 09:17:21 +0800

    add head

--- my-website/pages/about.tsx
+++ my-website/pages/about.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import MyNameCard from "../components/MyNameCard";
 import SkillCard from "../components/SkillCard";
@@ -5,6 +6,9 @@ import SkillCard from "../components/SkillCard";
 const about = () => {
   return (
     <Layout>
+      <Head>
+        <title>YuHsiang - Resume</title>
+      </Head>
       <MyNameCard />
       <SkillCard />
     </Layout>
--- my-website/pages/index.tsx
+++ my-website/pages/index.tsx
@@ -1,10 +1,11 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import WorkCard from "../components/WorkCard";
 import KindNav from "../components/KindNav";
-import { useEffect, useState } from "react";
 import config from "../config/file";
 import web from "../config/web";
-import Link from "next/link";
 import classes from "./index.module.sass";
 
 const Index = () => {
@@ -15,6 +16,21 @@ const Index = () => {
     <Layout>
       {/* <KindNav SetWorkKind={SetWorkKind} /> */}
       <div className={classes.container}>
+        <Head>
+          <title>YuHsiang - Portfolio</title>
+          <meta
+            property="og:url"
+            content="https://yuhsiang-work-b74t6x5tp-85223.vercel.app/"
+          />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="YuHsiang Portfolio" />
+          <meta property="og:description" content="YuHsiang Portfolio" />
+          <meta property="" />
+          <meta
+            property="og:image"
+            content="https://yuhsiang-work-b74t6x5tp-85223.vercel.app/S__4702210.jpg"
+          />
+        </Head>
         <div className={classes.root}>
           {config.web.map((item) => (
             <WorkCard
